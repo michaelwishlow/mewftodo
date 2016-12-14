f2bc58cfe9ae90a0adf413fec00291933bdb51cc
michaelwishlow <michael@pantheritconsulting.com>
2016-12-14 10:48:53 -0800

    Controllers

---
+++ app/controllers/todo_items_controller.rb
@@ -0,0 +1,19 @@
+class TodoItemsController < ApplicationController
+
+		def create
+			@todo_item = @todo_list.todo_items.create(todo_item_params)
+			redirect_to @todo_list
+		end
+
+		private
+
+		def set_todo_list
+			@todo_list = TodoList.find(params[:todo_list_id])
+		end
+
+		def todo_item_params
+			params[:todo_item].permit(:content)
+		end
+
+
+end
