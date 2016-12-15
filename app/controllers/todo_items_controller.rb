f4fceb76358855b3b3d003d6d4690782af9385b8
michaelwishlow <michael@pantheritconsulting.com>
2016-12-15 10:26:57 -0800

    Delete Function

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -6,6 +6,16 @@ class TodoItemsController < ApplicationController
 				redirect_to @todo_list
 		end
 
+		def destroy
+			@todo_item = @todo_list.todo_items.find(params[:id])
+			if @todo_item.destroy
+				flash[:success] = "Todo List Item was Deleted."
+			else
+				flash[:error] = "Todo List Item cold not be deleted."
+			end
+			redirect_to @todo_list
+		end
+
 		private
 
 		def set_todo_list
